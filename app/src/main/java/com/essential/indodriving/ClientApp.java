01d6f819a7cdc156f84d0fb813e805e5f2e4e051
Công Phương Đông <gongfangdong@gmail.com>
2016-02-20 17:52:07 +0700

    add tutorial

---
+++ app/src/main/java/com/essential/indodriving/ClientApp.java
@@ -0,0 +1,25 @@
+package com.essential.indodriving;
+
+import android.app.Application;
+
+import tatteam.com.app_common.AppCommon;
+import tatteam.com.app_common.sqlite.DatabaseLoader;
+
+/**
+ * Created by dongc_000 on 2/20/2016.
+ */
+public class ClientApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        AppCommon.getInstance().destroy();
+        DatabaseLoader.getInstance().destroy();
+
+        super.onTerminate();
+    }
+}
