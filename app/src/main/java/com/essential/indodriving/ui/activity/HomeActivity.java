7b91e461866dee47661989f0863b0f0028f5114f
ThanhNH <thanhnh.411@gmail.com>
2016-06-05 12:28:13 +0700

    - upgrade to pro

--- app/src/main/java/com/essential/indodriving/ui/activity/HomeActivity.java
+++ app/src/main/java/com/essential/indodriving/ui/activity/HomeActivity.java
@@ -15,6 +15,8 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.anjlab.android.iab.v3.BillingProcessor;
+import com.anjlab.android.iab.v3.TransactionDetails;
 import com.essential.indodriving.R;
 import com.essential.indodriving.data.DataSource;
 import com.essential.indodriving.ui.base.BaseConfirmDialog;
@@ -28,7 +30,7 @@ import tatteam.com.app_common.AppCommon;
 import tatteam.com.app_common.util.CloseAppHandler;
 import tatteam.com.app_common.util.CommonUtil;
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener, CloseAppHandler.OnCloseAppListener {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener, CloseAppHandler.OnCloseAppListener, BillingProcessor.IBillingHandler {
 
     public final static String PREF_IS_PRO_VERSION = "is_pro_version";
     public final static String PACKAGE_NAME_FREE_VER = "com.essential.indodriving.free";
@@ -46,6 +48,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     private FloatingActionButton mFabRateUs;
     private FloatingActionButton mFabMoreApps;
     private FloatingActionButton mFabShare;
+    private View button_pro_ver;
+    private View image_100_pro;
     private View mOverlayView;
     private CoordinatorLayout coordinatorLayout;
     private ImageView banner;
@@ -53,6 +57,8 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     private CloseAppHandler closeAppHandler;
     private int number;
     private boolean isProVersion;
+    private BillingProcessor billingProcessor;
+
     private View.OnClickListener mFloatingMenuItemClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -83,8 +89,12 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 public void onConfirmDialogButtonClick(BaseConfirmDialog.ConfirmButton button, @BaseConfirmDialog.DialogTypeDef int dialogType, BaseConfirmDialog dialog) {
                     switch (button) {
                         case OK:
-                            CommonUtil.openApplicationOnGooglePlay(HomeActivity.this, PACKAGE_NAME_PRO_VER);
                             dialog.dismiss();
+                            if (billingProcessor != null && billingProcessor.isInitialized()) {
+                                if (!billingProcessor.isPurchased(Constants.PURCHASE_PRO_VERSION_ID)) {
+                                    billingProcessor.purchase(HomeActivity.this, Constants.PURCHASE_PRO_VERSION_ID);
+                                }
+                            }
                             break;
                         case CANCEL:
                             dialog.dismiss();
@@ -106,20 +116,10 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         closeAppHandler = new CloseAppHandler(this, false);
         closeAppHandler.setListener(this);
         loadProState();
-        if (isProVersion) {
-            findViewById(R.id.button_pro_ver).setVisibility(View.GONE);
-            findViewById(R.id.image_100_pro).setVisibility(View.VISIBLE);
-        } else {
-            findViewById(R.id.button_pro_ver).setVisibility(View.VISIBLE);
-            findViewById(R.id.image_100_pro).setVisibility(View.GONE);
-            findViewById(R.id.button_pro_ver).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    UpgradeToProVerDialog dialog = new UpgradeToProVerDialog(HomeActivity.this);
-                    dialog.setOnConfirmDialogButtonClickListener(mOnConfirmDialogButtonClickListener);
-                    dialog.show();
-                }
-            });
+        refreshUI();
+
+        if (!isProVersion && BillingProcessor.isIabServiceAvailable(this)) {
+            billingProcessor = new BillingProcessor(this, Constants.DEV_KEY, this);
         }
     }
 
@@ -193,6 +193,9 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         mFabMoreApps = (FloatingActionButton) findViewById(R.id.fab_more_apps);
         mFabRateUs = (FloatingActionButton) findViewById(R.id.fab_rate_us);
         mFabShare = (FloatingActionButton) findViewById(R.id.fab_share);
+        button_pro_ver = findViewById(R.id.button_pro_ver);
+        image_100_pro = findViewById(R.id.image_100_pro);
+
         mOverlayView = findViewById(R.id.view_overlay);
         buttonLearnSimA.setOnClickListener(this);
         buttonLearnSimAUmum.setOnClickListener(this);
@@ -211,6 +214,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                         isProVersion = true;
                         Snackbar.make(coordinatorLayout, getString(R.string.hacked), Snackbar.LENGTH_SHORT).show();
                         saveProState();
+                        refreshUI();
                     }
                 }
             }
@@ -228,6 +232,15 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         mFabMoreApps.setOnClickListener(mFloatingMenuItemClickListener);
         mFabShare.setOnClickListener(mFloatingMenuItemClickListener);
         mFabRateUs.setOnClickListener(mFloatingMenuItemClickListener);
+
+        button_pro_ver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UpgradeToProVerDialog dialog = new UpgradeToProVerDialog(HomeActivity.this);
+                dialog.setOnConfirmDialogButtonClickListener(mOnConfirmDialogButtonClickListener);
+                dialog.show();
+            }
+        });
     }
 
     private void saveProState() {
@@ -242,6 +255,17 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         SharedPreferences sharedPreferences = getSharedPreferences(
                 Constants.SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
         isProVersion = sharedPreferences.getBoolean(PREF_IS_PRO_VERSION, false);
+    }
+
+    private void refreshUI() {
+        if (isProVersion) {
+            findViewById(R.id.button_pro_ver).setVisibility(View.GONE);
+            findViewById(R.id.image_100_pro).setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.button_pro_ver).setVisibility(View.VISIBLE);
+            findViewById(R.id.image_100_pro).setVisibility(View.GONE);
+        }
+
     }
 
     private void setFont(Typeface font) {
@@ -259,5 +283,46 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         String androidLink = "https://play.google.com/store/apps/details?id=" + getPackageName();
         String sharedText = getString(R.string.app_name) + ".\nAndroid: " + androidLink;
         CommonUtil.sharePlainText(this, sharedText);
+    }
+
+    @Override
+    public void onProductPurchased(String productId, TransactionDetails details) {
+        if (Constants.PURCHASE_PRO_VERSION_ID.equals(productId)) {
+            isProVersion = true;
+            saveProState();
+            refreshUI();
+        }
+    }
+
+    @Override
+    public void onPurchaseHistoryRestored() {
+
+    }
+
+    @Override
+    public void onBillingError(int errorCode, Throwable error) {
+
+    }
+
+    @Override
+    public void onBillingInitialized() {
+        isProVersion = billingProcessor.isPurchased(Constants.PURCHASE_PRO_VERSION_ID);
+        saveProState();
+        refreshUI();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (billingProcessor != null && !billingProcessor.handleActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (billingProcessor != null) {
+            billingProcessor.release();
+        }
+        super.onDestroy();
     }
 }
